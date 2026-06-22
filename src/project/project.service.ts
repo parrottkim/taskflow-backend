a03c579b3a3dc01fc7646c206ef4e469c6fb8df3
Parrottkim <parrottkim21@gmail.com>
2026-06-22 17:45:41 +0900

    feature: 도로명 주소 API 호출 로직 백엔드 이관
    close: #221

--- src/project/project.service.ts
+++ src/project/project.service.ts
@@ -159,12 +159,6 @@ export class ProjectService {
         break;
     }
 
-    if (value.isMine) {
-      queryBuilder.andWhere('manager.id = :currentUserId', {
-        currentUserId: user.id,
-      });
-    }
-
     // 6. 정렬 조건 설정
     const orderType = value.order?.toUpperCase() as 'ASC' | 'DESC';
 
